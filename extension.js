bfbfc3008eff60ed721f2f27cc68086393061d04
Oleg A. Khlybov <fougas@mail.ru>
2021-01-28 21:44:34 +0500

    Added required commands & options.

--- extension.js
+++ extension.js
@@ -25,12 +25,102 @@ function activate(context) {
 	});
 
 	vscode.commands.registerCommand('msys2.root', function () {
-		return "c:\\msys64";
+		return vscode.workspace.getConfiguration().get('msys2.root');
+	});
+
+	vscode.commands.registerCommand('msys2.bin', function () {
+		return vscode.commands.executeCommand('msys2.root').then(root => {
+			return `${root}\\bin`;
+		});
+	});
+
+	vscode.commands.registerCommand('msys2.cmake.exe', function () {
+		return vscode.commands.executeCommand('msys2.bin').then(bin => {
+			return `${bin}\\cmake.exe`;
+		});
+	});
+
+	vscode.commands.registerCommand('msys2.ninja.exe', function () {
+		return vscode.commands.executeCommand('msys2.bin').then(bin => {
+			return `${bin}\\ninja.exe`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw32.root', function () {
+		const mingw = vscode.workspace.getConfiguration().get('mingw32.root');
+		if(mingw == null) {
+			return vscode.commands.executeCommand('msys2.root').then(root => {
+				return `${root}\\mingw32`;
+			});
+		} else {
+			return mingw;
+		}
+	});
+
+	vscode.commands.registerCommand('mingw32.bin', function () {
+		return vscode.commands.executeCommand('mingw32.root').then(root => {
+			return `${root}\\bin`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw32.cc.exe', function () {
+		return vscode.commands.executeCommand('mingw32.bin').then(bin => {
+			return `${bin}\\gcc.exe`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw32.cxx.exe', function () {
+		return vscode.commands.executeCommand('mingw32.bin').then(bin => {
+			return `${bin}\\g++.exe`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw32.fc.exe', function () {
+		return vscode.commands.executeCommand('mingw32.bin').then(bin => {
+			return `${bin}\\gfortran.exe`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw64.root', function () {
+		const mingw = vscode.workspace.getConfiguration().get('mingw64.root');
+		if(mingw == null) {
+			return vscode.commands.executeCommand('msys2.root').then(root => {
+				return `${root}\\mingw64`;
+			});
+		} else {
+			return mingw;
+		}
+	});
+
+	vscode.commands.registerCommand('mingw64.bin', function () {
+		return vscode.commands.executeCommand('mingw64.root').then(root => {
+			return `${root}\\bin`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw64.cc.exe', function () {
+		return vscode.commands.executeCommand('mingw64.bin').then(bin => {
+			return `${bin}\\gcc.exe`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw64.cxx.exe', function () {
+		return vscode.commands.executeCommand('mingw64.bin').then(bin => {
+			return `${bin}\\g++.exe`;
+		});
+	});
+
+	vscode.commands.registerCommand('mingw64.fc.exe', function () {
+		return vscode.commands.executeCommand('mingw64.bin').then(bin => {
+			return `${bin}\\gfortran.exe`;
+		});
 	});
 
 	context.subscriptions.push(disposable);
 
-	console.log(vscode.commands.executeCommand('msys2.root'));
+	vscode.commands.executeCommand('mingw64.fc.exe').then(value => {
+		console.log(value);
+	});
 }
 exports.activate = activate;
 
